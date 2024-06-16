f666972dae5d5e4635cedfac9adbbdab48bcd269
Amalya Megerman <amalyamegerman@gmail.com>
2024-06-16 18:10:04 -0400

    fix: move bastion host to public subnet

--- main.tf
+++ main.tf
@@ -209,7 +209,7 @@ resource "aws_instance" "private_instance" {
 resource "aws_instance" "bastion_host" {
   ami                         = "ami-033fabdd332044f06"
   instance_type               = "t3.micro"
-  subnet_id                   = aws_subnet.private_subnet.id
+  subnet_id                   = aws_subnet.public_subnet.id
   associate_public_ip_address = true
   key_name                    = aws_key_pair.key.key_name
   vpc_security_group_ids      = [aws_security_group.dip2_bastion_host_sg.id]
