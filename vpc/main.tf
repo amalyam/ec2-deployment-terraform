a5f17b5111027d93c621dc76de397b59a48985dd
Amalya Megerman <amalyamegerman@gmail.com>
2024-06-17 18:02:46 -0400

    create vpc

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,3 +1,13 @@
 data "aws_availability_zones" "available" {
   state = "available"
 }
+
+resource "aws_vpc" "main" {
+  cidr_block       = var.cidr_block
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "dip-vpc"
+  }
+}
+
