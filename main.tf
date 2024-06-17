c1633c2872cc7009fa716c4dba41ac3d159ec3d6
Amalya Megerman <amalyamegerman@gmail.com>
2024-06-16 23:51:15 -0400

    add missing ingress from all traffic on port 80

--- main.tf
+++ main.tf
@@ -148,6 +148,12 @@ resource "aws_security_group" "dip2_public_sg" {
     cidr_blocks = [format("%s/32", jsondecode(data.http.myip.response_body).ip)]
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
