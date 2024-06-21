ac743df49d776a8109eb9586bdaca0f75889f7f0
Amalya Megerman <amalyamegerman@gmail.com>
2024-06-21 19:23:12 -0400

    add private route table and subnet association

--- vpc/main.tf
+++ vpc/main.tf
@@ -77,3 +77,23 @@ resource "aws_nat_gateway" "gw_nat" {
   depends_on = [aws_internet_gateway.gw]
 }
 
+resource "aws_route_table" "private_route_table" {
+  count  = 2
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.gw_nat[count.index].id
+  }
+
+  tags = {
+    Name = "$(var.app_name)-private-route-table-$(count.index + 1)"
+  }
+
+}
+
+resource "aws_route_table_association" "private_subnet_association" {
+  count          = 2
+  subnet_id      = aws_subnet.private_subnet[count.index].id
+  route_table_id = aws_route_table.private_route_table[count.index].id
+}
