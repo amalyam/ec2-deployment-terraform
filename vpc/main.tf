44f1b49ec2a752bb92b28d38c18cb7cb631ab755
Amalya Megerman <amalyamegerman@gmail.com>
2024-06-21 19:19:53 -0400

    add public route table and association

--- vpc/main.tf
+++ vpc/main.tf
@@ -40,3 +40,23 @@ resource "aws_internet_gateway" "gw" {
     Name = "$(var.app_name)-internet-gatway"
   }
 }
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "$(var.app_name)-public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  count          = 2
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
