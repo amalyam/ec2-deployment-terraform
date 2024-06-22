4ded5c862f881af9adccc031676bc7a74f6abbf8
Amalya Megerman <amalyamegerman@gmail.com>
2024-06-22 19:10:41 -0400

    remove from + to ports to fix InvalidParameterValue error

--- main.tf
+++ main.tf
@@ -113,8 +113,6 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tcp_traffic" {
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic" {
   security_group_id = aws_security_group.lb_sg.id
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 0
-  to_port           = 0
   ip_protocol       = "-1"
 }
 
