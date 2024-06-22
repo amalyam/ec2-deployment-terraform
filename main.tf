83e6df6eaaccbd647c6a574d59af7f2ef9c16b59
Amalya Megerman <amalyamegerman@gmail.com>
2024-06-22 19:07:13 -0400

    add load balancer and ingress/egress rules

--- main.tf
+++ main.tf
@@ -101,3 +101,61 @@ resource "aws_iam_instance_profile" "ec2_profile" {
   name = "${var.app_name}-ec2-profile"
   role = aws_iam_role.ec2_role.name
 }
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tcp_traffic" {
+  security_group_id = aws_security_group.lb_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic" {
+  security_group_id = aws_security_group.lb_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 0
+  to_port           = 0
+  ip_protocol       = "-1"
+}
+
+resource "aws_security_group" "lb_sg" {
+  name        = "${var.app_name}-lb-sg"
+  description = "Load balancer sg"
+  vpc_id      = module.vpc.vpc_id
+
+  tags = {
+    Name = "${var.app_name}-lb-sg"
+  }
+}
+resource "aws_lb" "load_balancer" {
+  name               = "${var.app_name}-load-balancer"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = module.vpc.public_subnet_ids
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name     = "${var.app_name}-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+}
+
+resource "aws_lb_target_group_attachment" "target_group_attachment" {
+  count            = 2
+  target_group_arn = aws_lb_target_group.target_group.arn
+  target_id        = aws_instance.private_instance[count.index].id
+  port             = 80
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.load_balancer.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+}
