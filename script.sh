367f42290e679fc593b240d0cd797cb641516faf
Amalya Megerman <amalyamegerman@gmail.com>
2024-06-22 20:46:42 -0400

    add styling and specific instance id to response

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 yum install nginx -y
-echo "Hello World" > /usr/share/nginx/html/index.html
+echo "<html><body><h1>&#128570;&#128075;</h1><p>Carrol the cat says hello from private instance ${instance_id}</p></body></html>" > /usr/share/nginx/html/index.html
 systemctl start nginx
