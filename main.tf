f03f65fd887a53a118955f7fbe76aaf2693e4e08
Amalya Megerman <amalyamegerman@gmail.com>
2024-06-26 13:55:32 -0400

    change port to 3000 and move lb resource

--- main.tf
+++ main.tf
@@ -99,6 +99,16 @@ resource "aws_iam_instance_profile" "ec2_profile" {
   role = aws_iam_role.ec2_role.name
 }
 
+resource "aws_security_group" "lb_sg" {
+  name        = "${var.app_name}-lb-sg"
+  description = "Load balancer sg"
+  vpc_id      = module.vpc.vpc_id
+
+  tags = {
+    Name = "${var.app_name}-lb-sg"
+  }
+}
+
 resource "aws_vpc_security_group_ingress_rule" "allow_tcp_traffic_lb" {
   security_group_id = aws_security_group.lb_sg.id
   cidr_ipv4         = "0.0.0.0/0"
@@ -113,15 +123,6 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_lb" {
   ip_protocol       = "-1"
 }
 
-resource "aws_security_group" "lb_sg" {
-  name        = "${var.app_name}-lb-sg"
-  description = "Load balancer sg"
-  vpc_id      = module.vpc.vpc_id
-
-  tags = {
-    Name = "${var.app_name}-lb-sg"
-  }
-}
 resource "aws_lb" "load_balancer" {
   name               = "${var.app_name}-load-balancer"
   internal           = false
@@ -141,7 +142,7 @@ resource "aws_lb_target_group_attachment" "target_group_attachment" {
   count            = 2
   target_group_arn = aws_lb_target_group.target_group.arn
   target_id        = aws_instance.private_instance[count.index].id
-  port             = 80
+  port             = 3000
 }
 
 resource "aws_lb_listener" "listener" {
